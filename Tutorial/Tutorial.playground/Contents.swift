6fa27b288e3222c4af728e7b6824658c419f51d4
Choi, Wanbok <wanbok.choi@linecorp.com>
2019-11-12 02:45:33 +0900

    Add Tutorial using playground

---
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+/*
+ [
+    {
+        "key": "266",
+        "name": "Aatrox",
+    ...
+    },
+    ...
+ ]
+ */
+let champsFilePath = Bundle.main.path(forResource: "champs", ofType: "json")
+
+/*
+ [
+    1,
+    33,
+    ...
+ ]
+ */
+let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", ofType: "json")
+
+let champsData = FileManager.default.contents(atPath: champsFilePath!)
+let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
+
+let champs = JSONSerialization.jsonObject(with: champsData!, options: [])
+let selectedIndexes = JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
+
+// TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
+let names: [String] = []
+print(names)
