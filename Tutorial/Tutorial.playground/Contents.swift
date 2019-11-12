d8edd1b2b05583e7729b139d29809b19b061dbcb
Seonghun <kim.seonghun23@gmail.com>
2019-11-12 20:02:39 +0900

    네번째 수정

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -28,17 +28,20 @@ let champs = try JSONSerialization.jsonObject(with: champsData!, options: []) as
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: []) as? [Int]
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
+
+let champsDict = champs?
+    .flatMap { [($0["key"] as? String) ?? "": ($0["name"] as? String) ?? ""] }
+    .reduce(into: [String: String](), { $0[$1.key] = $1.value })
+
 let names = selectedIndexes?
     .compactMap { String($0) }
-    .compactMap { index in
-        champs?.first { $0["key"] as? String == index }
-    }
-    .compactMap { $0["name"] as? String }
+    .compactMap { champsDict?[$0] }
     ?? []
 
 print(names)
 
-// 코드는 깔끔해 졌으나 챔프를 탐색할 때 중복하여 탐색하게 되는 문제가 있다.
-// 딕셔너리 배열이 아닌 딕셔너리로 탐색하면 비효율을 줄일 수 있을 것 같다.
-// 딕셔너리 배열을 딕셔너리로 변경하려면 어떻게 해야 할까?
-// 함수형으로 깔끔하게 변경하는 법을 좀 더 고민해야 겠다.
+// flatMap과 reduce를 이용하여 딕셔너리 배열을 딕셔너리로 변경하였다.
+// 그 결과 결과값을 바로 캐스팅하여 가져올 수 있게 되었다.
+// 이전 코드 보단 한층 깔끔한 느낌이다.
+// 다만 flatMap으로 튜플로 변경하는 부분의 코드가 가독성이 좋지 않고 지저분하다.
+// 저것도 더 깔끔하게 변경할 수 있을지 고민해 봐야 겠다.
