0145b9e6c9dfe8b106f14821bd09a98798a72490
nexmond <nexmond@twinny.co.kr>
2019-11-12 13:48:32 +0900

    해답 풀 리퀘스트

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -24,9 +24,12 @@ let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", o
 let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
-let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
-let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
+let champs = try JSONSerialization.jsonObject(with: champsData!, options: []) as! [[String:Any]]
+let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: []) as! [Int]
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
+let names: [String] = selectedIndexes.map{ index -> String in
+    return champs.filter{$0["key"] as! String == index.description}.first!["name"] as! String
+}
+
 print(names)
