75bc1b4e27b6f28336095bb4a220d56900577a2a
Jino0910 <rowkaxl@naver.com>
2019-11-12 13:52:16 +0900

    log champ

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -28,5 +28,17 @@ let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
+var names: [String] = []
+
+if let champsArray = champs as? Array<Any>, let selectedIndexesArray = selectedIndexes as? Array<Any> {
+
+    selectedIndexesArray.forEach { (index) in
+        if let index = index as? Int, champsArray.count >= index, let champ = champsArray[index] as? Dictionary<String, Any>, let name = champ["name"] as? String {
+            names.append(name)
+        }
+    }
+}
+
+
+
 print(names)
