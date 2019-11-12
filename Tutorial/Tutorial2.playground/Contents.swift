5d34f1fdc72131be078886c88afef27e3d03adf9
이광용 <rhkdrmfh@gmail.com>
2019-11-12 14:44:27 +0900

    Implement `uniqueMap`

--- Tutorial/Tutorial2.playground/Contents.swift
+++ Tutorial/Tutorial2.playground/Contents.swift
@@ -1,32 +1,40 @@
 import Foundation
 
-/*
- [
-    {
-        "key": "266",
-        "name": "Aatrox",
-    ...
-    },
-    ...
- ]
- */
+struct Champ: Decodable {
+    let key: String
+    let name: String
+}
+
+
 let champsFilePath = Bundle.main.path(forResource: "champs", ofType: "json")
 
-/*
- [
-    1,
-    33,
-    ...
- ]
- */
 let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", ofType: "json")
 
 let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
-let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
-let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
+let decoder = JSONDecoder()
+let champs = try decoder.decode([Champ].self, from: champsData!)
+let selectedIndexes = try decoder.decode([Int?].self, from: selectedIndexesData!)
 
-// TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
+extension Array where Element: Equatable {
+    func uniqueMap() -> Self {
+        return self.reduce([Element]()) {
+            return !$0.contains($1) ? ($0 + [$1]) : $0
+        }
+    }
+}
+
+let names: [String] = selectedIndexes
+    .uniqueMap()
+    .compactMap { $0 }
+    .map {
+        String(describing: $0)
+    }
+    .compactMap { key in
+        champs.first { $0.key == key }?.name
+    }
 print(names)
+
+
+
