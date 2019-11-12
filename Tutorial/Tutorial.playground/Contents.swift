6a02c98eacf93dd4b1505e269d64b50f0ce52bd5
MinyoungKoo <rnalsdud1014@naver.com>
2019-11-12 16:21:29 +0900

    챔피언 이름 출력
    - 고차함수를 사용해 key에 따라 챔피언의 이름을 출력한다.

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -28,5 +28,23 @@ let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
+let champsArray = champs as? [[String:Any]]
+let indexesArray = selectedIndexes as? [Int]
+
+var names: [String] = []
+
+indexesArray?.compactMap{ key in
+//    let champ = champsArray?.filter {
+//        $0["key"] as? String == "\(key)"
+//    }
+//
+//    champ?.forEach {
+//        names.append($0["name"] as? String ?? "")
+//    }
+    
+    champsArray?.filter { $0["key"] as? String == "\(key)" }
+        .forEach { names.append($0["name"] as? String ?? "")
+    }
+}
+
 print(names)
