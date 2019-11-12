3b51b645b3d925e30c553945bdf8f1ede18732ab
Mijin Jeon <mayihelpu27@gmail.com>
2019-11-12 14:15:26 +0900

    the first version

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -24,9 +24,32 @@ let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", o
 let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
-let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
-let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
-
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
+
+class Champion: Codable {
+    var key: String?
+    var name: String?
+}
+
+let champs = try! JSONDecoder().decode([Champion].self, from: champsData!)
+let indexes = try! JSONDecoder().decode([Int].self, from: selectedIndexesData!)
+
+//let numberIndexes = indexes.map{ Int($0)! }
+
+var names: [String] = []
+
+indexes.forEach { index in
+    champs.filter { champ in
+        champ.key == String(index)
+    }.map { names.append($0.name!) }
+}
+
+// ["Tryndamere", "Udyr", "Ahri", "Vayne", "Pyke"]
 print(names)
+
+//names = indexes.map { index in
+//    champs.filter { champ in
+//        champ.key == index
+//    }
+//}
+
