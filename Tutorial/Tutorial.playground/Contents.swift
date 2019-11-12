3dfb32237d81ba0aa1896111c25bd10af1a3817a
woookdev <hjw0911@gmail.com>
2019-11-12 14:10:11 +0900

    Fix one line print with reduce

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -27,25 +27,16 @@ let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFi
 let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
 
-
 if let unwrappedChamps = champs as? [[String: Any]],
   let selectedIndex = selectedIndexes as? [Int] {
-  selectedIndex.forEach { index in
-    
-    let filtered = unwrappedChamps.filter { champsDic -> Bool in
-      if let key = champsDic["key"] as? String {
-        if Int(key) == index {
-          return true
-        }
-        return false
-      }
-      return false
+  let result = unwrappedChamps.reduce("") { (first, value) -> String in
+    if selectedIndex.contains(Int(value["key"] as! String)!) {
+      return first + " " + (value["name"] as! String)
     }
-    
-    if !filtered.isEmpty, let name = filtered[0]["name"] {
-      print("Selected Champ Name : \(name)")
-    }
+    return first
   }
+  
+  print("This is Heros String : \(result)")
 }
 
 
