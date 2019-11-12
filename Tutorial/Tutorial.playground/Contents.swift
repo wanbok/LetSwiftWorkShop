7715a284959c5a06fbd1534c3814930aa738db6b
Byunsangjin <qustkdwls1@naver.com>
2019-11-12 14:00:56 +0900

    5명 이름 뽑기

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -25,8 +25,22 @@ let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
 let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
+
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
+
 let names: [String] = []
-print(names)
+
+if let champDic = champs as? [NSDictionary] {
+    var dates: [String:String] = [:]
+    for row in champDic {
+        if let key = row["key"] as? String, let name = row["name"] as? String {
+            dates[key] = name
+        }
+    }
+    
+    for index in selectedIndexes as! [Int] {
+        print("name is \(dates[String(index)]!)")
+    }
+}
