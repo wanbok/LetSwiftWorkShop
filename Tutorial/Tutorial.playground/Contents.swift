ebcebd04def11fb346fac5367767e7360fc1d010
denis <denis@dunamu.com>
2019-11-12 14:34:52 +0900

    - 1차 완료

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -24,9 +24,20 @@ let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", o
 let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
-let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
-let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
+let champs = try JSONSerialization.jsonObject(with: champsData!, options: []) as! [[String : Any]]
+let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])  as! [Int]
 
-// TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
-print(names)
+//// TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
+//champs.filter {
+//    let key = $0["key"]
+//
+//    return true
+//}
+champs.filter {
+    let keyNumer = Int($0["key"] as! String)
+    let selectedIndexList = selectedIndexes.filter { return $0 == keyNumer }
+    return selectedIndexList.count > 0 && keyNumer == selectedIndexList[0]
+}
+.forEach {
+    print($0["name"]!)
+}
