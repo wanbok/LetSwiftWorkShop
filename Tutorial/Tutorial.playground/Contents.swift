41eb3e034b06ad3ee0eef53bfe938f0caba5427c
Hyejun Jang <hyejun417@gmail.com>
2019-11-12 14:07:13 +0900

    순서별로 챔피언 이름 출력

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -28,5 +28,14 @@ let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
-print(names)
+
+if let selectedIndexes = selectedIndexes as? [Int], let champs = champs as? [[String: Any]] {
+    var names: [String?] = []
+    selectedIndexes.forEach { key in
+        names.append(
+            champs.filter { $0["key"] as? String == String(key) }
+                .first?["name"] as? String
+        )
+    }
+    print(names.compactMap { $0 })
+}
