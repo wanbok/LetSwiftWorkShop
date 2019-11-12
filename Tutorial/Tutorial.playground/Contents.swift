eef1ecc0feafc1b5ab25cfbb29ad0f0be1a8325d
Choi, Wanbok <wanbok.choi@linecorp.com>
2019-11-12 12:51:53 +0900

    Fix to add try to JSONserializer

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -24,8 +24,8 @@ let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", o
 let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
-let champs = JSONSerialization.jsonObject(with: champsData!, options: [])
-let selectedIndexes = JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
+let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
+let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
 let names: [String] = []
