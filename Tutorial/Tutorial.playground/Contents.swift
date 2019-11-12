47db93829281b8630d9b427e41e7c0fe7e26c141
jeong jin ho <yyyqor@gmail.com>
2019-11-12 14:12:27 +0900

    인덱스 별 챔프이름 뽑아내기

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -1,5 +1,8 @@
 import Foundation
-
+struct Champ: Codable {
+    var key: String
+    var name: String
+}
 /*
  [
     {
@@ -24,9 +27,23 @@ let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", o
 let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
-let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
+//let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
+let indexs = selectedIndexes as! [Int]
+let champs = try JSONDecoder().decode([Champ].self, from: champsData!)
+func getName(str: String) -> String? {
+    for json in champs {
+        let key = json.key
+        if key == str {
+            return json.name
+        }
+    }
+    return nil
+}
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
+let names: [String] =  indexs.map { (index) -> String in
+    let num = String(index)
+    return getName(str: num)!
+}
 print(names)
