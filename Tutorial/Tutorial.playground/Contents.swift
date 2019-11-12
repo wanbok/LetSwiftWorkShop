7fffe2215f9972b4001f65661bca87b46f999f1d
jeong jin ho <yyyqor@gmail.com>
2019-11-12 14:35:40 +0900

    플랫맵 써보기

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -31,19 +31,29 @@ let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFi
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
 let indexs = selectedIndexes as! [Int]
 let champs = try JSONDecoder().decode([Champ].self, from: champsData!)
+
 func getName(str: String) -> String? {
-    for json in champs {
-        let key = json.key
+    
+   
+    for champ in champs {
+        let key = champ.key
         if key == str {
-            return json.name
+            return champ.name
         }
     }
     return nil
 }
+func getName2(str: String) -> [String] {
+    
+   return champs.filter { str == $0.key}.map {$0.name}
+    
+}
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] =  indexs.map { (index) -> String in
-    let num = String(index)
-    return getName(str: num)!
+
+let names: [String] = indexs.compactMap {getName(str: String($0))}
+let names2: [String] = indexs.flatMap { (index) -> [String] in
+    getName2(str: String(index))
 }
 print(names)
+print(names2)
