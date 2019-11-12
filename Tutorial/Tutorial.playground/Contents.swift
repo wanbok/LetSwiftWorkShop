3440e7c9761ffe95071233359221d6fec657792a
JANGHUIJO <jhjo.tech@gmail.com>
2019-11-12 13:46:37 +0900

    챕피언 선택 번호에 따른 이름 배열에 넣기.

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -24,9 +24,14 @@ let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", o
 let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
-let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
-let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
+let champs = try JSONSerialization.jsonObject(with: champsData!, options: []) as? [[String: Any]]
+let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: []) as? [Int]
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
+var names: [String] = []
+selectedIndexes?.forEach{
+    let stringKey = String($0)
+    let champ = champs?.filter{ $0["key"] as? String == stringKey}
+    names.append(champ?.first?["name"] as! String)
+}
 print(names)
