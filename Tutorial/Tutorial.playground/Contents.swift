1c85eea55d0ab6cfd082887b62624cd892a9b490
jeong jin ho <yyyqor@gmail.com>
2019-11-12 14:48:38 +0900

    로직 함수 내 for loop 제거하기

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -32,28 +32,14 @@ let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData
 let indexs = selectedIndexes as! [Int]
 let champs = try JSONDecoder().decode([Champ].self, from: champsData!)
 
-func getName(str: String) -> String? {
-    
-   
-    for champ in champs {
-        let key = champ.key
-        if key == str {
-            return champ.name
-        }
-    }
-    return nil
+func getName(index: String) -> String? {
+    return champs.filter { String(index) == $0.key}.first?.name
 }
-func getName2(str: String) -> [String] {
-    
-   return champs.filter { str == $0.key}.map {$0.name}
-    
-}
+
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
 
-let names: [String] = indexs.compactMap {getName(str: String($0))}
-let names2: [String] = indexs.flatMap { (index) -> [String] in
-    getName2(str: String(index))
-}
+let names: [String] = indexs.compactMap {getName(index: String($0))}
+let names2: [String] = indexs.flatMap { (index) -> [String] in champs.filter { String(index) == $0.key}.map {$0.name}}
 print(names)
 print(names2)
