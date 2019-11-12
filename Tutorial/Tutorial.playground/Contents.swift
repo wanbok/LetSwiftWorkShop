a91e79c8069531c630fbe4066a5e30f5aa91506e
Everdeen <openmaya@gmail.com>
2019-11-12 14:29:09 +0900

    remove unused code

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -23,9 +23,6 @@ let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", o
 
 let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
-struct ReturnedData {
-    let name: String
-}
 let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
 
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
@@ -42,7 +39,7 @@ if let typedChamps = champs as? [[String: Any]] {
                 return indexes.contains(Int(champ["key"] as! String)!)
             }.map { champ in
                 return champ["name"] as! String
-            }.reduce
+            }
         names.append(contentsOf: extractedNames)
     }
 }
