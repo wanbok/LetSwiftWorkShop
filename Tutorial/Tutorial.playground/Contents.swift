59b974f3babaa73e5ab3a53a960fb651ac8446ec
kuu723 <seonmin723@gmail.com>
2019-11-12 14:19:59 +0900

    첫번째 리팩토링.

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -33,17 +33,25 @@ let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFi
 let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
 
+//if let champs = champs as? [[String: Any]], let selectedIndexes = selectedIndexes as? [Int] {
+//
+//    let result = selectedIndexes.map { (index) -> [String] in
+//        champs.filter { (dict) -> Bool in
+//            return (dict["key"] as? String)?.elementsEqual("\(index)") ?? false
+//        }.map({ cham in (cham["name"] as? String ?? "") })
+//    }
+//    print(result)
+//}
+
 let decoder = JSONDecoder()
 if let champsData = champsData,
     let champsResponse = try? decoder.decode([Cham].self, from: champsData),
     let selectedIndexes = selectedIndexes as? [Int] {
 
-    let value = selectedIndexes.map({ index in
-        return champsResponse.first { (cham) -> Bool in
-            return cham.key == "\(index)"
-        }
-    })
+    let name = selectedIndexes.compactMap({ "\($0)" })
+        .flatMap { (index) -> [Cham] in champsResponse.filter({ $0.key.elementsEqual(index) }) }
+        .map({ $0.name })
 
-    let names: [String] = value.map({ $0?.name ?? "" })
-    print("names: ", names)
+    print(name)
+
 }
