1441170dc094336fbe10c4d2dfb76a2cf4e3d9c8
도미닉 <kiljh@me.com>
2019-11-12 13:41:38 +0900

    테스트 완료

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -26,7 +26,19 @@ let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFi
 
 let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
+//구현
+struct Champion: Codable {
+    let key: String
+    let name: String
+}
+
+let champions = try! JSONDecoder().decode([Champion].self, from: champsData!).sorted(by: { (left, right) -> Bool in
+    return Int(left.key)! < Int(right.key)!
+})
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
+
+let names: [String] = champions.map { champion -> String in
+    return champion.name
+}
 print(names)
