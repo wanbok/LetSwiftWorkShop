85ec27f9c78623d5b8c01a90228d991812b92a16
denis <denis@dunamu.com>
2019-11-12 14:39:12 +0900

    2번째 방법

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -28,11 +28,7 @@ let champs = try JSONSerialization.jsonObject(with: champsData!, options: []) as
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])  as! [Int]
 
 //// TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-//champs.filter {
-//    let key = $0["key"]
-//
-//    return true
-//}
+/// 1번쨰 방법
 champs.filter {
     let keyNumer = Int($0["key"] as! String)
     let selectedIndexList = selectedIndexes.filter { return $0 == keyNumer }
@@ -40,4 +36,15 @@ champs.filter {
 }
 .forEach {
     print($0["name"]!)
+}
+    
+/// 2번쨰 방법
+selectedIndexes.forEach {
+    let index = "\($0)"
+    champs.filter {
+        return index == $0["key"] as! String
+    }
+    .forEach {
+        print($0["name"]!)
+    }
 }
