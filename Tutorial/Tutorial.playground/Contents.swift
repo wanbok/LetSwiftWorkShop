d230829974e98a19e3a999b3a4aefb6ab50ef82c
Jino0910 <rowkaxl@naver.com>
2019-11-12 14:31:02 +0900

    고계함수활용

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -28,17 +28,44 @@ let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-var names: [String] = []
+//var names: [String] = []
+//
+//if let champsArray = champs as? Array<Any>, let selectedIndexesArray = selectedIndexes as? Array<Any> {
+//
+//    selectedIndexesArray.forEach { (index) in
+//        if let index = index as? Int, champsArray.count >= index, let champ = champsArray[index] as? Dictionary<String, Any>, let name = champ["name"] as? String {
+//            names.append(name)
+//        }
+//    }
+//}
+//print(names)
 
-if let champsArray = champs as? Array<Any>, let selectedIndexesArray = selectedIndexes as? Array<Any> {
 
-    selectedIndexesArray.forEach { (index) in
-        if let index = index as? Int, champsArray.count >= index, let champ = champsArray[index] as? Dictionary<String, Any>, let name = champ["name"] as? String {
-            names.append(name)
+var names2: [String] = []
+
+if let champsArray = champs as? Array<Dictionary<String, Any>>, let selectedIndexesArray = selectedIndexes as? Array<Any> {
+    
+    print(
+        selectedIndexesArray.compactMap { (index) -> String? in
+            guard let index = index as? Int else { return nil }
+            guard champsArray.count > index else { return nil }
+            return String(index)
+            }
+            .compactMap ({ (key) -> Dictionary<String, Any>? in
+                return champsArray.first(where: { (champ) -> Bool in
+                    guard let champKey = champ["key"] as? String else { return false }
+                    return champKey == key
+                })
+            }).compactMap { (obj) -> String? in
+                
+                return obj["name"] as? String
         }
-    }
+    
+    )
+    
+    
+    
 }
+//print(names2)
 
 
-
-print(names)
--- Tutorial/Tutorial2.playground/Contents.swift
+++ Tutorial/Tutorial2.playground/Contents.swift
@@ -30,3 +30,29 @@ let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
 let names: [String] = []
 print(names)
+
+if let champsArray = champs as? Array<Dictionary<String, Any>>, let selectedIndexesArray = selectedIndexes as? Array<Any> {
+    
+    print(
+        selectedIndexesArray.compactMap { (index) -> String? in
+            guard let index = index as? Int else { return nil }
+            guard champsArray.count > index else { return nil }
+            
+            return String(index)
+            }
+            //        .flatMap{$0}
+            .compactMap ({ (key) -> Dictionary<String, Any>? in
+                return champsArray.first(where: { (champ) -> Bool in
+                    guard let champKey = champ["key"] as? String else { return false }
+                    return champKey == key
+                })
+            }).compactMap { (obj) -> String? in
+                
+                return obj["name"] as? String
+        }
+        
+    )
+    
+    
+    
+}
