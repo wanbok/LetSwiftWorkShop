0dc4e4c53a76cfc35896204ec8fb02c192db4815
Seonghun <kim.seonghun23@gmail.com>
2019-11-12 13:46:19 +0900

    1차 수정 버전

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -28,5 +28,25 @@ let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
 let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
+var names: [String] = []
+
+let champList = (champs as? [[String: Any]]) ?? []
+let selectedIndexList = (selectedIndexes as? [Int]) ?? []
+
+let temp = champList
+    .filter {
+        guard let index = Int(($0["key"] as? String) ?? "") else { return false }
+        return selectedIndexList.contains(Int(($0["key"] as? String) ?? "") ?? -1)
+}
+
+selectedIndexList.forEach { index in
+    let value = temp.filter {
+        guard let indexValue = Int(($0["key"] as? String) ?? "") else { return false }
+        return index == indexValue
+    }
+        .compactMap { $0["name"] as? String }
+        .first
+    names.append(value ?? "")
+}
+
 print(names)
