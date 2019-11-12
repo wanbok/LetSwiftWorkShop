d34f7598d80c2d6599394864b0e60a0ffd839e23
JANGHUIJO <jhjo.tech@gmail.com>
2019-11-12 14:36:13 +0900

    고계 함수를 사용해서 코드 정리.

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -29,9 +29,14 @@ let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
 var names: [String] = []
-selectedIndexes?.forEach{
+
+names = selectedIndexes?.map {
     let stringKey = String($0)
-    let champ = champs?.filter{ $0["key"] as? String == stringKey}
-    names.append(champ?.first?["name"] as! String)
+    return champs?
+        .filter{ $0["key"] as? String == stringKey }
+        .compactMap{ $0["name"] as? String }
+        .first
 }
+.compactMap{ $0 } ?? []
+
 print(names)
