6bf5ff688876893ff9c23decd5a26dc70cc34f1b
도미닉 <kiljh@me.com>
2019-11-15 21:35:52 +0900

    강제 언레핑 제거

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -25,7 +25,7 @@ let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
 let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
-let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: []) as! [Int]
+let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: []) as? [Int] ?? []
 //구현
 struct Champion: Codable {
     let key: String
@@ -37,12 +37,12 @@ let zero = 0
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
 
 let names: [String] = try! JSONDecoder().decode([Champion].self, from: champsData!)
-                                        .filter({ champion -> Bool in
-                                            selectedIndexes.contains(Int(champion.key)!)
-                                        })
-                                        .sorted(by: { (left, right) -> Bool in
+                                        .filter() { champion -> Bool in
+                                            selectedIndexes.contains(Int(champion.key) ?? zero)
+                                        }
+                                        .sorted() { (left, right) -> Bool in
                                             return selectedIndexes.firstIndex(of: Int(left.key) ?? zero) ?? zero < selectedIndexes.firstIndex(of: Int(right.key) ?? zero) ?? zero
-                                        })
+                                        }
                                         .map { champion -> String in
                                             return champion.name
                                         }
