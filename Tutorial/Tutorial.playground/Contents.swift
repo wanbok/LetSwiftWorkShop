2a9f48568bde579cc70a09967e1765cd25d73059
도미닉 <kiljh@me.com>
2019-11-15 21:29:38 +0900

    1번 예제 피드백 반영

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -32,15 +32,18 @@ struct Champion: Codable {
     let name: String
 }
 
-let champions = try! JSONDecoder().decode([Champion].self, from: champsData!).sorted(by: { (left, right) -> Bool in
-    return Int(left.key)! < Int(right.key)!
-}).filter({ champion -> Bool in
-    selectedIndexes.contains(Int(champion.key)!)
-})
+let zero = 0
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
 
-let names: [String] = champions.map { champion -> String in
-    return champion.name
-}
+let names: [String] = try! JSONDecoder().decode([Champion].self, from: champsData!)
+                                        .filter({ champion -> Bool in
+                                            selectedIndexes.contains(Int(champion.key)!)
+                                        })
+                                        .sorted(by: { (left, right) -> Bool in
+                                            return selectedIndexes.firstIndex(of: Int(left.key) ?? zero) ?? zero < selectedIndexes.firstIndex(of: Int(right.key) ?? zero) ?? zero
+                                        })
+                                        .map { champion -> String in
+                                            return champion.name
+                                        }
 print(names)
