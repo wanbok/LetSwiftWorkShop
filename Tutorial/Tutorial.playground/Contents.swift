b4d2e0b280b5ad45c2387cbe0fe741a48c49f918
Seonghun <kim.seonghun23@gmail.com>
2019-11-15 19:17:08 +0900

    코드 개선 및 익명함수 사용하지 않도록 수정

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -24,25 +24,34 @@ let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", o
 let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
-let champs = try JSONSerialization.jsonObject(with: champsData!, options: []) as? [[String: Any]]
-let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: []) as? [Int]
-
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
 
-let champsDict = champs?
-    .flatMap { [$0["key"] as? String: $0["name"] as? String] }
-    // 옵셔널 값을 그대로 딕셔너리에 키값으로 넣었다. 지금은 오류가 없으나 추가적인 사이드 이팩트가 어떤게 있을까?
-    .reduce(into: [:]) { $0[$1.key] = $1.value }
+struct Champ: Decodable {
+    let key: String
+    let name: String
+}
 
-let names = selectedIndexes?
-    .compactMap { String($0) }
-    .compactMap { champsDict?[$0] }
-    ?? []
+func merge(to dict: inout [String: String], from champ: Champ) {
+    dict[champ.key] = champ.name
+}
+
+func champName(at key: Int) -> String? {
+    return champDict["\(key)"]
+}
+
+let champs = try JSONDecoder().decode([Champ].self, from: champsData ?? Data())
+let selectedIndexes = try JSONDecoder().decode([Int].self, from: selectedIndexesData ?? Data())
+
+let champDict = champs
+    .reduce(into: [:], merge)
+// 처음엔 Champ의 init에서 Dictionary로 정보를 받아 처리해 주도록 하여 개선해 보았으나 고계함수의 활용과 거리가 멀어보여 JSONDecoder를 활용하여 개선하도록 수정하였다.
+
+let names = selectedIndexes
+    .compactMap(champName)
+// Int를 String으로 변환해 주던 함수를 생략하였다.
+// Int를 String으로 변환해 줄 뗀 옵셔널 타입이 아니라 compactMap을 사용할 필요가 없다.
 
 print(names)
 
-// flatMap과 reduce를 이용하여 딕셔너리 배열을 딕셔너리로 변경하였다.
-// 그 결과 결과값을 바로 캐스팅하여 가져올 수 있게 되었다.
-// 이전 코드 보단 한층 깔끔한 느낌이다.
-// 다만 flatMap으로 튜플로 변경하는 부분의 코드가 가독성이 좋지 않고 지저분하다.
-// 저것도 더 깔끔하게 변경할 수 있을지 고민해 봐야 겠다.
+// JSONDecoder를 활용하여 처리하도록 변경하였다.
+// 그리고 익명함수를 안쓰고 적용할 수 있도록 변경해 보았다.
